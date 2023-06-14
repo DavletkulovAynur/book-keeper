09c1db41e557182d50dcb2848566bed16caf0aa5
afdavletkulov <afdavletkulov@rttv.ru>
2023-06-13 22:53:51 -0300

    New module

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import { Routing } from "pages";
 import "./index.scss";
 import { withProviders } from "./providers";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const App = () => {
   return (
     <div>
-      <Routing />
+      <Provider store={store}>
+        
+        <Routing />
+      </Provider>
     </div>
   );
 };
