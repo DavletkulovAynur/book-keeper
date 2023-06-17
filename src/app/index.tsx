987a998ed3cd700909ff3fd2637641cfa767e12c
afdavletkulov <afdavletkulov@rttv.ru>
2023-06-17 13:55:19 -0300

    add ud-ui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,13 +4,20 @@ import "./index.scss";
 import { withProviders } from "./providers";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { Layout } from "antd";
+import { Header, Sidebar } from "modules/ud-ui";
 
 const App = () => {
   return (
     <div>
       <Provider store={store}>
-        
-        <Routing />
+        <Layout>
+          <Sidebar />
+          <Layout>
+            <Header />
+            <Routing />
+          </Layout>
+        </Layout>
       </Provider>
     </div>
   );
