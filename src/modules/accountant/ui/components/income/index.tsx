76be6b26142c7a8379e8489e6463f17469b236aa
afdavletkulov <afdavletkulov@rttv.ru>
2023-07-13 20:43:45 -0300

    feature-test1

--- src/modules/accountant/ui/components/income/index.tsx
+++ src/modules/accountant/ui/components/income/index.tsx
@@ -1,13 +1,18 @@
 import React, { memo } from 'react'
 import { Form } from 'antd'
 import { MainTable } from './Table'
+import { EditModal } from './EditModal'
 
 const Income: React.FC = memo(() => {
   const [form] = Form.useForm()
+
+  //TODO: modal
+  
  
   return (
     <Form form={form} component={false}>
       <MainTable form={form}/>
+      <EditModal />
     </Form>
   )
 })
